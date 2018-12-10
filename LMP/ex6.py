a39da09fe8244c2147a0b4f0f3affb3df19b11e4
adilrk <adilrk@gmail.com>
2018-12-09 21:56:28 -0800

    adding change

--- LMP/ex6.py
+++ LMP/ex6.py
@@ -3,21 +3,29 @@
 import argparse
 import glob
 import os
+import sys
 parser = argparse.ArgumentParser(description='this my find commad')
 parser.add_argument('start',nargs=1,help='file name or pattern')
 parser.add_argument('-name',help='file name or pattern')
 parser.add_argument('-print',help='list the file path')
 parser.add_argument('-type',help='file type directory(d) or file(f) ')
 args=parser.parse_args()
-print args.start[0]
-print args
 filelist=os.listdir(args.start[0])
 if args.name:
     for filename in filelist:
         if args.name in  filename:
             if args.type:
                 if args.type == 'd':
-                    if os.isdir(filename):
+                    full_path = os.path.join(args.start[0],filename)
+                    #print full_path
+                    if os.path.isdir(full_path):
                         print filename
-
-            #print filename
+                elif args.type == 'f':
+                    full_path = os.path.join(args.start[0],filename)
+                    if os.path.isfile(full_path):
+                        print filename
+                else:
+                    print "type not supported"
+                    sys.exit()
+            else:
+                print filename
